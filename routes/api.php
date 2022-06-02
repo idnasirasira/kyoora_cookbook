5e55569192ed1a854d7f00371afdebdf8f5d203c
Aris Arisandi <idnasirasira@gmail.com>
2022-06-02 12:18:27 +0700

    Fix routing & welcome page

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout'])->name('api.logout');
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/recipes', [RecipeController::class, 'index'])->name('recipe.index');
