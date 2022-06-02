4daec8e2eb4c1ef0ecd38a564e721a6fa0840342
Aris Arisandi <idnasirasira@gmail.com>
2022-06-02 12:04:28 +0700

    eh lupa gais

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    static function checkEmailExists($email)
+    {
+        $instance = new static;
+        return !$instance->where('email', $email)->get()->isEmpty();
+    }
 }
